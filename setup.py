5a22f94163458940f72ebab4431b2db6a1e21a56
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2021-04-05 10:08:44 -0700

    fix(setup.py): named package exports, package metadata

--- setup.py
+++ setup.py
@@ -1,25 +1,37 @@
 import os
+import re
 from setuptools import setup, find_packages
 
-import ced
+def find_version(filename):
+    _version_re = re.compile(r'__version__ = "(.*)"')
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
 
-def read(sr_file):
-    with open(os.path.join(os.path.dirname(__file__), sr_file)) as dh_file:
-        return dh_file.read()
+version = find_version("opl/__init__.py")
+
+packages = find_packages(exclude=("examples*", "test*", "scrap*", "build*"))
 
 setup(
-    name='ced',
-    version=ced.__version__,
-    description='CED Python Library',
+    name='opl',
+    version=opl.__version__,
+    description='Open-MBEE Python Library',
     author='Blake Regalia',
     author_email='blake.d.regalia@jpl.nasa.gov',
-    url='https://github.jpl.nasa.gov/OpenCAE/ced-python-lib',
-    keywords=['CED'],
+    url='https://github.com/Open-MBEE/opl',
+    keywords=['OPL'],
     include_package_data=True,
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    packages=['ced'],
+    packages=['opl'],
     install_requires=read('requirements.txt').strip().split('\n'),
+    extras_require={
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-apidoc",
+        ],
+    },
     license='Apache 2.0',
     classifiers=[
         'Programming Language :: Python :: 3',
